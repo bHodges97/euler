23bdeabd42fd3a6f69a8db512a16cdba70ac23bd
Benjamin Hodges <benjaminhodges07@gmail.com>
2020-09-19 15:48:28 +0100

    Fraction free solution

--- euler65.py
+++ euler65.py
@@ -1,8 +1,6 @@
 #approximate e
 
 from math import e,gcd
-from fractions import Fraction
-
 
 def twok(i):
     if (i+1) % 3 == 0:
@@ -10,13 +8,14 @@ def twok(i):
     else:
         return 1
 
-x = 0
-for i in range(99,0,-1):
-    x = Fraction(1,(twok(i) + x))
+target = 100
+n,d = 1,twok(target-1)
+for i in range(target-2,0,-1):
+    n,d = d , d * twok(i) + n
 
-solution =2+x
-print(solution,e)
-print(sum(int(x) for x in str(solution.numerator)))
+n,d = 2*d+n,d
+print(n/d,e)
+print(sum(int(x) for x in str(n)))
 
 
 
