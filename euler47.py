1f5237d9e5ce5997c8b74ef14837430283f3de83
Benjamin Hodges <benjaminhodges07@gmail.com>
2020-09-21 00:34:10 +0100

    ignoring 100+ solutions

--- euler47.py
+++ euler47.py
@@ -1,17 +1,5 @@
- 
-file = open("primes.txt","r")
-primes  = [int(x) for x in file.read().split(',')]
-file.close()
+from primes import pfactorise
 
-def pfactors(n,l=[]):
-    for i in primes:
-         if n%i==0:
-             l.append(i)
-             a = n/i
-             if a == 1:
-                 return l
-             else:   
-                 return pfactors(a,l)  
 l1 = 0
 l2 = 0
 l3 = 0
@@ -20,7 +8,7 @@ for i in range(646, 199999900):
     l1 = l2
     l2 = l3
     l3 = l4
-    fact = set(pfactors(i,[]))
+    fact = set(pfactorise(i,[]))
     l4 = len(fact)
     if l1 == l2 == l3 == l4 == 4:
         print(i-3)
