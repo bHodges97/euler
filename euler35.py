ba8f3931bd964ac0a04a4d20e3d75f1b727e74eb
Benjamin Hodges <benjaminhodges07@gmail.com>
2020-09-21 23:44:57 +0100

    rewrote code for 35 to be cleaner

--- euler35.py
+++ euler35.py
@@ -1,38 +1,36 @@
 #notes
 #https://oeis.org/A068652 <-list of circular primes
-#2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97 
+#2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97
 #this solution takes roughly 1 min :(.
 #on second thought ignoring ones that contain even numbers or 5
 #made this take less than 1 second to solve :)
 
 def solve():
-    file = open("primes.txt","r")
-    primes  = [int(x) for x in file.read().split(',')]
-    file.close()
-    r = primes[:]
-   # r = [x for x in r if x < 100]
-    rotations = []
-    total = 0
-    for i in r:
-        if i in rotations:
-            continue
-        string = str(i)
-        if '2' in string or '5' in string or '4' in string or '6' in string or '8' in string or '0' in string:
-            continue
-        flag = True
-        temp = []
-        for digit in range(0,len(string)):
-            string = string[1:]+string[:1]
-            integer = int(string)
-            if integer not in primes:
-                flag = False;
-                break
-            elif flag and integer not in rotations and integer not in temp:
-                temp.append(integer)
-        if flag:
-            total+=len(temp)
-            rotations.extend(temp)
-            print(total," ",i)
-    return len(rotations);
+   with open("primes.txt","r") as f:
+      primes = [int(x) for x in f.read().split(",")]
+
+   limit = 1000000
+   primes = [x for x in primes if x < limit]
+   circular = [0] * limit
+
+
+   for idx,prime in enumerate(primes):
+      if circular[prime]:
+         continue
+      if prime < 10:
+         circular[prime] = 1
+         continue
+
+      prime_str = str(prime)
+      digits = (int(x) for x in prime_str)
+      is_circular = all(x%2!=0 and x!=5 for x in digits)
+
+      rotations = [prime_str[x:] + prime_str[:x] for x in range(len(prime_str))]
+      rotations = [int(x) for x in rotations if x[0] != '0']
+      is_circular = is_circular and all(x in primes for x in rotations)
+
+      for x in rotations:
+         circular[x] = is_circular
+   return sum(circular)
 print(solve())
-	    
+
