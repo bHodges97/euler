076dac6104e526df52cd59f2f03796471da1f744
Benjamin Hodges <benjaminhodges07@gmail.com>
2020-09-22 01:09:49 +0100

    Fix euler37 solution
    The original solution doesn't produce right answer.
    New solution is about 18 secs to run, feels like improvements could be
    made somewhere.

--- euler37.py
+++ euler37.py
@@ -1,30 +1,47 @@
 #as per usual the answer is on wikipedia and the and eois
 #https://oeis.org/A020994
+from time import time
+
 
 def solve():
-    file = open("primes.txt","r")
-    primes  = [int(x) for x in file.read().split(',')]
-    file.close()
-    r = primes[4:]
-    sum = 0
-    for i in r:
-        left = str(i)
-        if '2' in left or '4' in left or '5' in left or '6' in left or '8' in left or '0' in left:
-            continue
-        right = left
-        k = len(left)
-        flag = False
-        for j in range(0,k-1):
-            left=left[1:]
-            if int(left) not in primes:
-                flag = True
-                break
-            right=right[:-1]
-            if int(right) not in primes:
-                flag = True
-                break
-        if not flag:
-            sum+=i
-    return sum;
-print(solve())
-	    
+    with open("primes.txt","r") as f:
+        primes  = [int(x) for x in f.read().split(',')]
+
+    count = 0
+    ans = 0
+    left_set = {'2','3','5','7'}
+    right_set = {'2','3','5','7'}
+
+    while count != 11:
+        left_tmp = set()
+        right_tmp = set()
+
+        #optimisation here maybe?
+        for i in range(1,10):
+            i = str(i)
+            for x in left_set:
+                left = i+x
+                if int(left) in primes:
+                    left_tmp.add(left)
+                    #print(left)
+
+        #right sight cannot end with 2 or 5
+        for i in ['1','3','7','9']:
+            for x in right_set:
+                right = x+i
+                if int(right) in primes:
+                    right_tmp.add(right)
+        left_set = left_tmp
+        right_set = right_tmp
+
+        both_set = left_set & right_set
+        if both_set:
+            print(both_set)
+        count+=len(both_set)
+        ans+=sum(int(x) for x in both_set)
+        #print(count)
+    return ans
+
+t = time()
+print("solution:",solve())
+print("time:",time()-t)
