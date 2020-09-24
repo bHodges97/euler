d9a2c3803bd64d3f612e09ee409e187301b3d675
Benjamin Hodges <benjaminhodges07@gmail.com>
2020-09-24 19:27:35 +0100

    euler60 for bigger primes but still too slow

--- euler60.py
+++ euler60.py
@@ -4,50 +4,15 @@ from itertools import combinations
 from primes import *
 from math import ceil
 
-target = 4
 
 
-def in_graph(a,b,graph):
-    return ((a,b) if a < b else (b,a)) in graph
-
-def k_cliques(graph):
-    print("hi")
-    vertices = set()
-    degrees = defaultdict(int)
-
-    for v in graph:
-        for i in v:
-            degrees[i]+=1
-
-    vertices = {i for i,j in degrees.items() if j>target}
-
-    cliques_v = {(i,j) for i,j in graph}
-    cliques = [[tuple(x)] for x in cliques_v]
-    k = 2
-
-    while cliques:
-        yield k, cliques_v
-
-        cliques_1 = set()
-        for clique in cliques_v:
-            for v in vertices:
-                if all(in_graph(v,x,graph) for x in clique):
-                    new_clique = tuple(sorted(list(clique)+[v]))
-                    cliques_1.add(new_clique)
-
-        cliques_v = cliques_1
-        k += 1
-
-def solve():
+def solve(target):
     pset = frozenset(primes)
     edges = set()
-    print(len(primes))
 
     def isprime(pstr):
         p = int(pstr)
-        if pstr[0] == '0':
-            return False
-        elif p < primes[-1]:
+        if p < primes[-1]:
             return p in pset
         else:
             lim = ceil(p ** 0.5)
@@ -58,29 +23,60 @@ def solve():
                     return True
             print("Ran out of primes ya fool!")
             exit()
+    #2-cliques,3-cliques...k-cliques
+    k_cliques = [set() for x in range(target-1)]
+    edges = defaultdict(bool)
+    connected = defaultdict(int)
+    graph = set()
+    verts = set()
 
-    for x in primes:
-        x = str(x)
-        for j in range(1,len(x)):
-            a,b = x[:j], x[j:]
-            if a!=b and isprime(a) and isprime(b) and isprime(b+a):
-                a,b = sorted([int(a),int(b)])
-                edges.add((a,b))
-    edges = frozenset(edges)
-    print("edge gen ok",len(edges))
+    for idx,b in enumerate(primes):
+        x = str(b)
+        new_verts = set()
+        for a in primes[:idx]:
+            y = str(a)
 
-    for k,c in k_cliques(edges):
-        print(k,"clique done")
-        if k == target:
-            c = ((sum(x),x) for x in c)
-            c = sorted(c,key=lambda x:x[0])
+            if isprime(x+y) and isprime(y+x):
+                edges[(a,b)] = True
+                connected[a]+=1
+                connected[b]+=1
 
-            print(c[:4])
-            break
+                if connected[a] >= target-1:
+                    new_verts.add(a)
+                    f = True
 
-        print("----")
+                if connected[b] >= target-1:
+                    new_verts.add(b)
+                    f = True
+
+                graph.add((a,b))
+
+        if new_verts:
+            verts |= new_verts
+            k_cliques_new = [set() for x in range(target-1)]
+            graph_tmp = set()
+            for a,b in graph:
+                if a in verts and b in verts:
+                    graph_tmp.add((a,b))
+            graph -= graph_tmp
+            k_cliques[0] |= graph_tmp
 
 
+            for q in new_verts:
+                #k-1 ... 0
+                for i in range(len(k_cliques)-2,-1,-1):
+                    for clique in k_cliques[i]:
+                        if all(edges[(x,q)] for x in clique):
+                            new_clique = tuple(list(clique)+[q])
+                            k_cliques_new[i+1].add(new_clique)
 
-solve()
+                for cliques,cliques_new in zip(k_cliques,k_cliques_new):
+                    cliques |= cliques_new
+
+                if k_cliques[-1]:
+                    x = k_cliques[-1].pop()
+                    print(x,sum(x))
+                    #return sum(x)
+                    exit()
+print(solve(5))
 
